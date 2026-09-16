cd9c41fecce73cba0126c7aa9f32c516c041eed5
lu chen <lc45011285@gmail.com>
2026-09-17 00:49:56 +0800

    服务初始化

---
+++ gateway-service/src/main/java/com/wzh/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.wzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * API网关
+ *
+ * @author luchen
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
